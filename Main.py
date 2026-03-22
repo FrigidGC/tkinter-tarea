27598fdad7dc66dfb0bca39c778c6bcf4639e2c2
FrigidGC <guerrerocervantesandres@gmail.com>
2026-03-22 16:03:53 -0600

    Logica inicial de las bolas que rebotan

--- Main.py
+++ Main.py
@@ -1,10 +1,12 @@
 import tkinter as tk
-ventana = tk.Tk()
-ventana.title("tarea-taller-grafico")
-ventana.geometry("800x800")
-ventana.resizable(width=False, height=False)
+from Ballgraphic import GraficoBolas
+#Ventana principal
+root = tk.Tk()
+root.title("tarea-taller-grafico")
+root.geometry("800x800")
+root.resizable(width=False, height=False)
 
-canva1 = tk.Canvas(ventana, bg="yellow", width=800, height=800)
+canva1 = tk.Canvas(root, bg="yellow", width=800, height=800)
 canva1.pack()
 
-ventana.mainloop()
+root.mainloop()
